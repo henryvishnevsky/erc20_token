d62e5c5f79863ac937c96b730ffa9b7442a29000
henryWiszniewski <github.m67uk@passmail.net>
2023-10-16 15:33:18 -0600

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,11 +8,17 @@ contract Token {
 	string public symbol;
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
-	//uint256 public totalSupply = 1000000 * (10**decimals);
+	
+	// Track balances 
+	mapping (address => uint256) public balanceOf;
+	// Send tokens
+
+
 
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
 		name = _name;
 		symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
 	}
 }
--- test/Token.js
+++ test/Token.js
@@ -7,12 +7,16 @@ return ethers.utils.parseUnits(n.toString(), 'ether')
 
 describe('Token', () => {
     
-    let token
+    let token, accounts, deployer
 
 	beforeEach(async() => {
 	// Fetch Token from Blockchain
 	const Token = await ethers.getContractFactory('Token')
-  	token = await Token.deploy('Dapp university', 'DAPP' , '1000000')	
+  	token = await Token.deploy('Dapp university', 'DAPP' , '1000000')
+
+  	accounts = await ethers.getSigners()
+  	deployer = accounts[0]	
+
 	})     
     
    	describe('Deployment', () => {
@@ -20,25 +24,30 @@ describe('Token', () => {
    		const symbol = 'DAPP'
  		const decimals = '18'
  		const totalSupply = tokens('1000000')
- 			
+
    	 // Check that name is correct
 	it('has correct name', async () => {
-	expect(await token.name()).to.equal(name)	
+	  	expect(await token.name()).to.equal(name)	
 	})
 
     // Check that symbol is correct
 	it('has correct symbol', async () => {	 	
-	expect(await token.symbol()).to.equal(symbol)	
+		expect(await token.symbol()).to.equal(symbol)	
 	})
 
     // Check that it has correct decimals
 	it('has correct decimals', async () => {	 	
-	expect(await token.decimals()).to.equal(decimals)	
+		expect(await token.decimals()).to.equal(decimals)	
 	})
 
 	 // Check that it has correct total supply
 	it('has correct total sypply', async () => {	
-	expect(await token.totalSupply()).to.equal(totalSupply)	
+		expect(await token.totalSupply()).to.equal(totalSupply)	
+	})	
+
+	 // Check that total supply is assigned to deployer
+	it('assigns total supply to deployer', async () => {	
+		expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)	
 	})	
 
    	})
